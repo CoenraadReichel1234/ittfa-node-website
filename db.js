b9fe756969c09e7c8d06f6f27f25ea9f0bd238c8
Coenraad Reichel <coeniereichel@gmail.com>
2024-08-14 16:43:57 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const mysqlPool = mysql.createPool({
-    host: 'monorail.proxy.rlwy.net',
-    user: 'root',
-    password: 'rLperAKkADurKtrvRiMCroBMULVQbAlS',
-    database: railway,
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 module.exports = mysqlPool;
